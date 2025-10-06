065281099792353aed8208046b27096ce90b5ef4
ZaizafunHanifah <hanifah9158@gmail.com>
2025-10-06 17:57:31 +0700

    styling homepage

---
+++ app/src/main/java/com/infoin/ui/main/CategoryAdapter.kt
@@ -0,0 +1,4 @@
+package com.infoin.dummy
+
+// Placeholder to avoid duplicate class during the migration of layouts.
+object CategoryAdapterDummy { }
---
+++ app/src/main/java/com/infoin/ui/main/ExploreFragment.kt
@@ -0,0 +1,5 @@
+package com.infoin.dummy
+
+// This file was converted to a harmless placeholder to avoid duplicate class declarations
+// The real ExploreFragment implementation lives under com.octanews.infoin.ui.main
+object ExploreFragmentDummy { }
